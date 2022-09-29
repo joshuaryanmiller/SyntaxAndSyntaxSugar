e6d4098305ee7f0b901f67ebe36376f7e0d4232f
Joshua Miller <joshua.ryan.miller@icloud.com>
2022-09-29 09:58:10 -0700

    initial commit

---
+++ SyntaxAndSyntaxSugar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SyntaxAndSyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //inferred typing, string interpolation, and the ternary operator
+
+            var answer = 10;
+
+            var response = (answer < 9)? $"{answer}is less than 9." : $"{answer} is greater than or equal to 9.";
+            Console.WriteLine(response);
+        }
+    }
+}
